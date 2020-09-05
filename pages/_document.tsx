aaf5c9995d936674b6c2cab0f6b728aa02982076
Wildiney Di Masi <wildiney@gmail.com>
2020-09-04 21:09:25 -0300

    fix: structure review

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,16 @@
+import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class CustomDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head></Head>
-                <body>
-                    <Main></Main>
-                </body>
-                <NextScript></NextScript>
-            </Html>
-        )
-    }
+  render () {
+    return (
+      <Html>
+        <Head></Head>
+        <body>
+          <Main></Main>
+        </body>
+        <NextScript></NextScript>
+      </Html>
+    )
+  }
 }
