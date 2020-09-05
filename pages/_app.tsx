de559fddbd8b62f88d6c069ef286fc445b7e7327
Wildiney Di Masi <wildiney@gmail.com>
2020-09-04 22:01:19 -0300

    chore: test api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.node,
+  pageProps: PropTypes.any
 }
 
 export default MyApp
