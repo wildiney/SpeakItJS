736d178b98f2613711536c7aa9cfc937d59d4c6e
Wildiney Di Masi <wildiney@gmail.com>
2022-05-30 18:12:42 -0300

    refactor: convert to flexbox

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import classes from './Header.module.css'
 
-function Header() {
+function Header () {
   return (
     <header className={classes.header}>
       <h1>Speak It</h1>
