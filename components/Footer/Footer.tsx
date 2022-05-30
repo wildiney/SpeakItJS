46e508e4945c08ed5c3179f73b5e691144948074
Wildiney Di Masi <wildiney@gmail.com>
2022-05-30 18:04:02 -0300

    refactor: move footer to fixed position

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import classes from './Footer.module.css'
 
-function Footer() {
+function Footer () {
   return (
     <footer className={classes.footer}>
-      <p>&copy; 2020 - slicedpixel.com</p>
+      <p>&copy; 2022 - slicedpixel.com</p>
     </footer>
   )
 }
