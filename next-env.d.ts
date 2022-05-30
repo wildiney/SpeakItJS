77caa8563dc75f07aefb7a3a104a91f29802795d
Wildiney Di Masi <wildiney@gmail.com>
2022-05-30 19:06:35 -0300

    refactor: update structure

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="next" />
-<<<<<<< HEAD
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-=======
-/// <reference types="next/types/global" />
->>>>>>> 7580bb9b62e2dc9d158929b9f684ea8daecc226a
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 // import App from 'next/app'
 import '../styles/globals.css'
 
