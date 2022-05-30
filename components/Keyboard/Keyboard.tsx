21e78c16bdc8d6bd14495db5a6652b1c1226848f
Wildiney Di Masi <wildiney@gmail.com>
2022-05-30 18:02:06 -0300

    refactor: remove unused keys from keyboard

--- components/Keyboard/Keyboard.tsx
+++ components/Keyboard/Keyboard.tsx
@@ -7,48 +7,48 @@ import classes from './Keyboard.module.css'
 
 function Keyboard ({ addLetter, speakHandler }) {
   const letters = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-    '˜',
-    '´',
-    ',',
-    '.',
-    '!',
-    '?',
-    '/',
-    ' ',
-    'apagar',
-    'enter'
+    // 'a',
+    // 'b',
+    // 'c',
+    // 'd',
+    // 'e',
+    // 'f',
+    // 'g',
+    // 'h',
+    // 'i',
+    // 'j',
+    // 'k',
+    // 'l',
+    // 'm',
+    // 'n',
+    // 'o',
+    // 'p',
+    // 'q',
+    // 'r',
+    // 's',
+    // 't',
+    // 'u',
+    // 'v',
+    // 'w',
+    // 'x',
+    // 'y',
+    // 'z',
+    // '˜',
+    // '´',
+    // ',',
+    // '.',
+    // '!',
+    // '?',
+    // '/',
+    // ' ',
+    // 'apagar',
+    // 'enter'
   ]
 
   return (
     <div className={classes.keyboard}>
       <div className={classes.keys}>
-        {letters.map((letter) => {
+        {/* {letters.map((letter) => {
           return (
             <Button key={letter}
               clickHandler={() => {
@@ -63,7 +63,7 @@ function Keyboard ({ addLetter, speakHandler }) {
               {letter === ' ' ? 'ESPAÇO' : letter}
             </Button>
           )
-        })}
+        })} */}
         <Button clickHandler={speakHandler} newclass="btn__fullsize">LEIA!</Button>
       </div>
     </div>
